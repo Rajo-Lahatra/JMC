c167776ff18149aa3f18846a04c0b95cfa3f564a
Rajo-Lahatra <lahatrajo@gmail.com>
2025-09-29 17:09:24 +0000

    style: harmonisation des couleurs avec la charte du logo

--- src/components/MissionsList.tsx
+++ src/components/MissionsList.tsx
@@ -1,4 +1,5 @@
 // src/components/MissionsList.tsx
+import './MissionsList.css'
 import { useState, useEffect } from 'react'
 import { supabase } from '../lib/supabaseClient'
 import type { Mission, Collaborator } from '../types'
